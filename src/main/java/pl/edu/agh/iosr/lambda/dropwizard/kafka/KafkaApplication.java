095079a9ac5be73f4e98e855ef243c9f80e739bf
PuszekSE <puszekse@gmail.com>
2015-05-25 23:12:38 +0200

    Kafka producer

--- src/main/java/pl/edu/agh/iosr/lambda/dropwizard/kafka/KafkaApplication.java
+++ src/main/java/pl/edu/agh/iosr/lambda/dropwizard/kafka/KafkaApplication.java
@@ -1,15 +1,46 @@
 package pl.edu.agh.iosr.lambda.dropwizard.kafka;
 
+import kafka.javaapi.producer.Producer;
+
+
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
 import storm.trident.tuple.TridentTuple;
+
+import java.util.Properties;
+import java.util.logging.Logger;
 
 /**
  * Created by Puszek_SE on 2015-05-23.
  */
 public class KafkaApplication {
 
-    //TODO sending actual tupples to kafka messaging system
-    public boolean sendTuple(TridentTuple tuple){
+    Logger logger = Logger.getLogger(this.getClass().getName());
 
-        return false;
+    //TODO SET BROKER ADDRESS
+    String broker = "SET_ME";
+
+    Producer<String,TridentTuple> producer;
+
+    public KafkaApplication(){
+        Properties props = new Properties();
+
+        props.put("metadata.broker.list",broker);
+        props.put("request.required.acks","1");
+
+        ProducerConfig config = new ProducerConfig(props);
+
+        producer = new Producer<>(config);
+
+    }
+
+    public boolean sendTuple(TridentTuple tuple){
+        try{
+            producer.send(new KeyedMessage<String, TridentTuple>("tuple",tuple));
+        }catch (Exception e){
+            logger.warning("Message failed: "+e);
+            return false;
+        }
+        return true;
     }
 }
